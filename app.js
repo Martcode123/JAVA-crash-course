8b3c7f274e385065d001f6253f9915e5ad7396d7
Martins Richard <112550418+Martcode123@users.noreply.github.com>
2022-09-20 17:01:23 +0100

    click me

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 // let count = 1
 
 // while (count <= 100) {
 //     console.log(count)
 //   count = count + 1
 // }
-  
+
 // for (let i = 1; i <= 20; i++) {
 //     if (i % 3 === 0 && i % 5 === 0) {
 //         console.log(`${i} -> frontend simplified`)
@@ -19,15 +18,14 @@
 //     else {
 //         console.log(`${i} -> ${i}`)
 //     }
-    
-    
+
 // }
 
 // const str = "frontend simplified"
 
 // for (let i = 0;  i < str.length; ++i) {
 //     console.log(str[i])
-     
+
 // }
 
 // Function definition
@@ -48,7 +46,6 @@
 // })
 
 // console.log(newArr)
-
 
 // let grades = ['A+', 'A', 'FAIL']
 // let goodgrades = grades.filter((element) => {
@@ -74,7 +71,6 @@
 //     return true
 // })
 
-
 // console.log(goodgrades)
 
 // let grades = ['A+', 'A', 'FAIL']
@@ -98,7 +94,6 @@
 
 // console.log(NewArray)
 
- 
 //  let dollars = [1, 5, 10, 3]
 
 //  dollars.map((element) => {
@@ -119,42 +114,58 @@
 
 // console.log(cents)
 
-let users = [ 
-{
-    username: 'martins',
-    email: 'martins@frontendsimplified.com',
-    subscriptionStatus: 'VIP',
-    password: 'test123'
-},
+//  let users = [
+//  {
+//      username: 'martins',
+//      email: 'martins@frontendsimplified.com',
+//      subscriptionStatus: 'VIP',
+//      password: 'test123'
+// },
 
-];
+// ];
 
-function login(email, password) {
-    for (let i = 0; i < users.length; i++) {
-     if (users[i].email === email) {
-       console.log(users[i]);
-       if (users[i].password === password) {
-           console.log('log the user in - the details are correct')
-        }
-        else {
-            console.log('password is incorrect - try again')
-        }
-        return;
-     } 
-        
-    }
-    console.log('could not find an email that matches')
+//  function login(email, password) {
+//          for (let i = 0; i < users.length; i++) {
+//       if (users[i].email === email) {
+//         console.log(users[i]);
+//         if (users[i].password === password) {
+//             console.log('log the user in - the details are correct')
+//          }
+//          else {
+//              console.log('password is incorrect - try again')
+//          }
+//          return;
+//       }
+
+//      }
+//      console.log('could not find an email that matches')
+//  }
+
+//  login('martins@frontendsimplified.com', 'test123')
+
+
+
+// function register(user) {
+//     users.push(user);
+// }
+
+
+// register({
+//     username: "rich", 
+//     email: "rich@fes.com", 
+//     password: "rich123", 
+//     subscriptionStatus: "VIP", 
+//     discordId: "rich#0001", 
+//     lessonsCompleted: [0, 1]
+// });
+    
+
+// console.log(users)
+
+function toggleDarkMode() {
+    document.querySelector('body').classList.toggle("dark-theme")
 }
 
-login('martins@frontendsimplified.com', 'test123')
 
-
-
-
-
-
-
-    
- 
 
 
